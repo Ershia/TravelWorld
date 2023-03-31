9f22660bb08b7628a9f35cd2cf9e9f9c25c3698a
Ershia <ershia94@gmail.com>
2023-03-31 08:44:55 +0200

    navigation

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require(“express”);
+const cors = require(“cors”);
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.get('/',(req,res) => {
+    res.send("<h1>Hello World</h1>")
+});
+app.listen(PORT,() => {
+    console.log('Server is running on PORT')
+});
